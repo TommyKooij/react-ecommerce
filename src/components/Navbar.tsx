7ef3ae70078235790866d9d2ad9f905013dc38cb
Tom Kooij <74959713+TommyKooij@users.noreply.github.com>
2026-06-23 12:42:19 +0200

    23/06/2026

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,17 +4,40 @@ export default function Navbar() {
   return (
     <nav className="navbar">
       <div className="navbar-container">
-        <Link to="/" className="navbar-brand">
+        <Link
+          to="/"
+          className="navbar-brand"
+        >
           ShopHub
         </Link>
         <div className="navbar-links">
-          <Link to="/">Home</Link>
-          <Link to="/checkout">Cart</Link>
+          <Link
+            to="/"
+            className="navbar-link"
+          >
+            Home
+          </Link>
+          <Link
+            to="/checkout"
+            className="navbar-link"
+          >
+            Cart
+          </Link>
         </div>
         <div className="navbar-auth">
           <div className="navbar-auth-links">
-            <Link to="/auth">Login</Link>
-            <Link to="/auth">Sign In</Link>
+            <Link
+              to="/auth"
+              className="btn btn-secondary"
+            >
+              Login
+            </Link>
+            <Link
+              to="/auth"
+              className="btn btn-primary"
+            >
+              Sign In
+            </Link>
           </div>
         </div>
       </div>
